fddccc402a258842ffaefe28b39129d582928a56
Jimin Jeon <jimin@Jiminui-MacBookAir.local>
2026-01-30 15:22:45 -0500

    fix: UI BUG FIX

--- dextel/ur3_realsense_hamer.py
+++ dextel/ur3_realsense_hamer.py
@@ -99,8 +99,8 @@ class RobustTracker:
     def init_realsense(self):
         self.pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
         
         profile = self.pipeline.start(config)
         stream = profile.get_stream(rs.stream.depth)
